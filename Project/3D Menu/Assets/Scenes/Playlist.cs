61aa1521146073c773381d4a5e95481530353ba3
mtmarci <mtmarci@gmail.com>
2015-10-18 18:50:21 +0200

    Basics

---
+++ Project/3D Menu/Assets/Scenes/Playlist.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Playlist : MonoBehaviour {
+	PlaylistItem[] PLI;
+
+	Playlist(){
+
+	
+	}
+
+	void add(){
+
+
+	}
+
+	void remove(){
+
+
+	}
+
+	void getNoOfItems(){
+
+
+	}
+
+	PlaylistItem getNext(int count){
+
+		return PLI [count + 1];
+	}
+}
---
+++ Project/3D Menu/Assets/Scenes/PlaylistItem.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaylistItem : MonoBehaviour {
+
+	PlaylistItem(){
+
+
+	}
+}
